4215c40a17e6ad2b47884069c70581a6da5561b4
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2021-10-27 13:59:07 +0200

    Fixed loop and printing to .txt file.

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-console.log('Hello Dante.');
+const fs = require('fs');
 
+function extract() {
+    let rawData = fs.readFileSync('swe.json');
+    let data = JSON.parse(rawData);
+    let images = data['imgs'];
+
+    const file = fs.createWriteStream('companies.txt', { flags: 'a'});
+
+    // loop images
+    for (let i = 0; i < images.length; i++) {
+        console.log(images[i]['attr']['alt']);
+        file.write(images[i]['attr']['alt'].toString() + '\r\n');
+    }
+}
+
+extract();
